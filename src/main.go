88788aa2bca2a2613c5532316527367be22dbc7f
Christopher <cbwestl91@gmail.com>
2013-04-04 16:44:55 +0200

    main file added

---
+++ src/main.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+	"fmt"
+	"fmt"
+	"src/elevdriver.go"
+)
+
+func main(){
+	//establish connection to UDP "I am alive" port
+
+	//check if master exists
+
+	//if master exists, run slave()
+
+	//if no master, check if highest checksum, if so run master()	
+}
+
+func slave()
+
+func master()
+
+func local() //??
