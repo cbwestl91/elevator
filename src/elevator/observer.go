23f902dcb1cebcd7aefc58a67b0567e00f477230
redbullnerd <knuthvamb@hotmail.com>
2013-04-05 12:07:58 +0200

    Created elevator files

---
+++ src/elevator/observer.go
@@ -0,0 +1,23 @@
+
+// Here functions that checks for stopbutton, obstruction and floor will be declared
+
+package elevator
+
+import (
+	"elevdriver"
+	"fmt"
+	"time"
+)
+
+func observeFloor() {
+
+}
+
+func observeStopbutton () {
+
+}
+
+func observeObstruction () {
+
+}
+
