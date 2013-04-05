e9ea237888d59ef2d880accea1c4e7024cb67888
Heislab <victor.iversen@gmail.com>
2013-04-05 09:24:56 +0200

    working on network

--- src/main.go
+++ src/main.go
@@ -8,9 +8,9 @@ import(
 func main(){
 	//establish connection to UDP "I am alive" port
 
-	//check if master exists
+	//run slave(), check if master exists
 
-	//if master exists, run slave()
+	//
 
 	//if no master, check if highest checksum, if so run master()	
 }
--- src/network/network.go
+++ src/network/network.go
@@ -5,9 +5,16 @@ import(
 	"net"
 )
 
+var(
+	localIP = "129.241.187.142"
+	broadcast = "129.241.187.255" //må se nærmere på adressen
+	
+	UDPport = "8769"
+)
 
-func EstablishConnection(multicastAddr, multicastPort) {
-	mcAddr, err = net.ResolveUDPAddr("udp", multicastAddr:multicastPort)
+func ListenforMaster(multicastAddr, multicastPort) {
+	destination = broadcast + ":" + UDPport
+	mcAddr, err = net.ResolveUDPAddr("udp", destination)
 	_, _ = net.ListenMulticastUDP("udp", nil, mcAddr)
 
 }
