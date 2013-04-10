5f5e7e7b56674f76a3d6437846c1a325ce48bc3f
Christopher <cbwestl91@gmail.com>
2013-04-10 12:15:41 +0200

    wednsday is middle of the week

--- src/network/TCP.go
+++ src/network/TCP.go
@@ -22,49 +22,60 @@ func TCPconnectionHandler() {
 
 }
 
+// OUTPUTS: TCPmap over GETCURRENTMAP
+//	    TCPmap[wantedIP] over GETSINGLECONN
+// INPUTS:  newMapEntry from UPDATETCPMAP
+//	    deadIP from ISDEADCHAN
+//	    bool from GETCURRENTMAP
+//	    
 func mapOverseer() {
 	TCPmap := make(map[string]net.Conn)
 	for {
 		select {
-		case newIP := <- newIPchan:
-			_, exists := TCPmap[newIP]
-			if !exists {
-				go connectTCP(newIP)
-			} else {
-				fmt.Println("This IP already exists in map.. which is weird")
-			}
-		case deadIP := <- isDeadchan:
-
+		case newMapEntry := <- updateTCPmap // new entry detected
+			TCPmap[newMapEntry.IP] = newMapEntry.socket
+		case deadIP := <- isDeadchan: // someone stopped transmitted UDP, and needs to be removed from map
+			delete(TCPmap, deadIP)
+		case <- giveMeCurrentMap: // send function wants full map
+			getCurrentMap <- TCPmap
+		case wantedIP := <- giveMeConn: // if only one connection is wanted
+			getSingleConn <- TCPmap[wantedIP]
+		}
+	}
 }
 
+// OUTPUTS: newMapEntry over UPDATETCPMAP
 func listenTCP(){ // listens for TCP connections 
 	destination := ":" + TCPport
 	addr, err := net.ResolveTCPAddr("tcp", destination)
 	if err != nil {
 		fmt.Println("error resolving TCP adress")
 	} else {
-		listener, err := net.ListenTCP("tcp", addr)
-		fmt.Println("listening for new TCP connections")
-		if err != nil {
-			fmt.Println("error listening for TCP connections")
-		} else {
-			socket, err := listener.Accept()
+		for {
+			listener, err := net.ListenTCP("tcp", addr)
+			fmt.Println("listening for new TCP connections")
 			if err != nil {
-				fmt.Println("error accepting TCP connection")
+				fmt.Println("error listening for TCP connections")
 			} else {
-				remoteElevAddr := socket.RemoteAddr().String()
+				socket, err := listener.Accept()
+				if err != nil {
+					fmt.Println("error accepting TCP connection")
+				} else {
+					remoteElevAddr := socket.RemoteAddr().String()
 				
-				remoteElevSplitter := strings.Split(remoteElevAddr, ":")
-				remoteElevIP := remoteElevSplitter[0]
-				newMapEntry := TCPconnection{socket, remoteElevIP}
+					remoteElevSplitter := strings.Split(remoteElevAddr, ":")
+					remoteElevIP := remoteElevSplitter[0]
+					newMapEntry := TCPconnection{socket, remoteElevIP}
 
-				// found new peer. will forward info about peer
-				updateTCPmap <- newMapEntry
-			} // what happens if several identical copies are made? overwrite?
+					// found new peer. will forward info about peer
+					updateTCPmap <- newMapEntry
+				} // what happens if several identical copies are made? overwrite?
+			}
 		}
 	}
 }
 
+// OUTPUTS: newMapEntry over UPDATETCPMAP
 func connectTCP(remoteIP string) {
 	destination := remoteIP + ":" + TCPport
 	addr, err := net.ResolveTCPAddr("tcp", destination)
@@ -83,7 +94,13 @@ func connectTCP(remoteIP string) {
 	}
 }
 
-func sendTCP(communicator commChannels){ // for sending information over one or all TCP connections
+// OUTPUTS: true over GIVEMECURRENTMAP
+// 	    input.IP over GIVEMECONN
+// INPUTS:  input struct from COMMUNICATOR.SENDTOALL
+//	    TCPmap from GETCURRENTMAP
+//	    input struct from COMMUNICATOR.SENDTOONE
+//	    socket from GETSINGLECONN
+func sendTCP(communicator commChannels){ 
 	for { // communication is done over channels, so function should never return
 		select {
 			case: input := <- communicator.sendToAll:
@@ -109,7 +126,8 @@ func sendTCP(communicator commChannels){ // for sending information over one or 
 	}
 }
 
-func (conn TCPconnection) receiveTCP(communicator commChannels){ //temptxt: kalles med conn.receiveTCP(communicator)
+// OUTPUTS: messages received over MESSAGERECEIVEDCHAN
+func (conn TCPconnection) receiveTCP(communicator commChannels){
 	var msg [512]byte
 	for {
 		_, err := conn.socket.Read(msg[0:])
