6c70f547833b7156c3fe6d23431274ea5ee9d301
Christopher <cbwestl91@gmail.com>
2013-04-12 12:46:12 +0200

    network module should work

--- src/networkTEST.go
+++ src/networkTEST.go
@@ -8,21 +8,19 @@ import(
 func main() {
 	var communicator network.CommChannels
 	communicator.CommChanInit()
-	
+	network.NetworkInit(communicator)
+
 	time.Sleep(time.Second)
 	
 	go sendTESTmail(communicator)
 
 	for {
-		newMessage := <- communicator.MessageReceivedchan
 		time.Sleep(time.Millisecond)
 	}
 }
 
 func sendTESTmail(communicator network.CommChannels) {
 	for {
-		message := network.Message{content: []byte("test")}
-		communicator.SendToAll <- message
 		time.Sleep(time.Second)
 	}
 }
